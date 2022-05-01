9dcc95b2eb09721f0f365bf6e7c695b0485cd34a
vova <martiniden0@gmail.com>
2022-05-01 23:37:22 +0300

    fix add_user uri

--- speaking/speaking/urls.py
+++ speaking/speaking/urls.py
@@ -20,7 +20,7 @@ from rest_framework import routers
 from mainpage import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'api/users', views.UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
